cdea244e2cbf604a4fbdbab95951057c719a5423
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-03-03 22:47:26 +0200

    ohio

--- springboot/springbootapp/src/main/java/org/example/controller/impl/UserAccountControllerImpl.java
+++ springboot/springbootapp/src/main/java/org/example/controller/impl/UserAccountControllerImpl.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/reddit")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserAccountControllerImpl implements UserAccountController {
 
     @Autowired
--- springboot/springbootapp/src/main/java/org/example/controller/impl/UserActionsControllerImpl.java
+++ springboot/springbootapp/src/main/java/org/example/controller/impl/UserActionsControllerImpl.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/reddit")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserActionsControllerImpl implements UserActionsController {
     @Autowired
     private UserActionsServiceImpl userActionsService;
